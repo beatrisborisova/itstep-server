220fbf029465c3ab05d6bb4e1a9709cb4cfef58f
Beatris Borisova <beti16b@gmail.com>
2023-04-24 00:41:09 +0300

    db changes

--- database/db.js
+++ database/db.js
@@ -4,14 +4,14 @@ require('dotenv').config()
 module.exports = async () => {
     try {
         mongoose.connect(process.env.DB_CONNECTION)
+        mongoose.connection.on('connected', () => {
+            console.log('Database connected');
+        })
 
-        console.log('Database connected');
-
+    } catch (err) {
         mongoose.connection.on('error', (err) => {
             console.error('Database error');
             console.error(err);
         })
-    } catch (err) {
-        console.log('Error connecting database');
     }
 }
