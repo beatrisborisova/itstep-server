6917e7aab7fe8d4d7b45886100efce1cebc8f90d
Beatris Borisova <beti16b@gmail.com>
2023-04-24 01:03:51 +0300

    Added cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const router = require('./routes/routes');
 const database = require('./database/db');
+const cors = require('cors');
 require('dotenv').config()
 
 const port = parseInt(process.env.PORT);
 
 app.use(express.json())
 app.use('/', router);
+app.use(cors());
 
 const start = async () => {
     await database(app);
